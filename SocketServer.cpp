3152973cd5159cb3f1ed22bd97dbff4be640f8f3
MythMyth <windy10091996@gmail.com>
2022-03-28 20:45:59 +0700

    ?

--- SocketServer.cpp
+++ SocketServer.cpp
@@ -1,20 +1,41 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#define PORT 8080
+using namespace std;
 
 int main() {
     std::cout << "Start server\n";
     int server_fd, new_socket, valread;
-    struct sockaddr_in address;
     int opt = 1;
-    int addrlen = sizeof(address);
     char buffer[1024] = {0};
-
+    // Creating socket fd
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if(server_fd == 0) {
         std::cout << "Socket failed\n";
         return 1;
     }
-    
+    // Set socket option
+    if(setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) == -1) {
+        cout << "Set socket option failed\n";
+        return 1;
+    }
+
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    address.sin_addr = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+    //Bind socket to port
+    if( bind(server_fd, (struct socketaddr*)&address, addrlen) < 0) {
+        cout << "Bind error \n";
+        return 1;
+    }
+
+    if(listen(server_fd, 5) < 0) {
+        cout << "Listen error \n";
+        return 1;
+    }
+
     return 0;
 }
